65ce27055edb6981ccbcbe4e72e71bebdb4f3a62
Alexander Gudkov <alexandergudkov@Alexanders-MBP.attlocal.net>
2021-02-03 01:54:51 -0600

    delete seeds line

--- app.js
+++ app.js
@@ -50,7 +50,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
-// seedDB();
 
 app.use(function(req, res, next){
 	res.locals.currentUser = req.user;
